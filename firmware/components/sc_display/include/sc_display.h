7ba96998a2d96562915d33e56c6f0313be986517
durki <michal@durkalec.com>
2024-04-20 22:32:03 +0200

    Ported example code to work with updated drivers

--- firmware/components/sc_display/include/sc_display.h
+++ firmware/components/sc_display/include/sc_display.h
@@ -1,7 +1,6 @@
-//
-//
-
 #ifndef SMART_COMPASS_SC_DISPLAY_H
 #define SMART_COMPASS_SC_DISPLAY_H
+
+void sc_display_init();
 
 #endif //SMART_COMPASS_SC_DISPLAY_H
--- firmware/main/main.c
+++ firmware/main/main.c
@@ -2,11 +2,14 @@
 #include "nvs_flash.h"
 
 #include "sc_ble.h"
+#include "sc_display.h"
 
 
 void
 app_main(void)
 {
-    ESP_LOGI("main", "Hello World");
+    ESP_LOGI("main", "BLE init");
     sc_ble_init();
+    ESP_LOGI("main", "Display init");
+    sc_display_init();
 }
