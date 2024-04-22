79046b25a31a792edc4bded5e362b26efda3514a
durki <michal@durkalec.com>
2024-04-22 14:49:25 +0200

    Display code refactoring

--- firmware/components/sc_display/sc_display.c
+++ firmware/components/sc_display/sc_display.c
@@ -1,3 +1,4 @@
+#include <sys/cdefs.h>
 #include "sc_display.h"
 
 #include <stdlib.h>
@@ -8,101 +9,61 @@
 #include "freertos/task.h"
 #include "esp_freertos_hooks.h"
 #include "freertos/semphr.h"
+
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
 #include "lvgl.h"
 #else
 #include "lvgl/lvgl.h"
 #endif
-
 #include "lvgl_helpers.h"
 #include "widgets/lv_img.h"
 #include "arrow.h"
 
-/*********************
- *      DEFINES
- *********************/
 #define TAG "sc_display"
 #define LV_TICK_PERIOD_MS 1
 
-/**********************
- *  STATIC PROTOTYPES
- **********************/
+_Noreturn static void guiTask(void *pvParameter);
 static void lv_tick_task(void *arg);
+static void create_ui(void);
+static void ui_refresh_task();
 
-static void guiTask(void *pvParameter);
-
-static void lv_example_get_started_1(void);
-static void angle_refresh_task();
-
-/**********************
- *   APPLICATION MAIN
- **********************/
 void sc_display_init() {
     ESP_LOGI(TAG, "Init");
-
-    /* If you want to use a task to create the graphic, you NEED to create a Pinned task
-     * Otherwise there can be problem such as memory corruption and so on.
-     * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
     xTaskCreatePinnedToCore(guiTask, "gui", 4096 * 2, NULL, 0, NULL, 1);
 }
 
-/* Creates a semaphore to handle concurrent call to lvgl stuff
- * If you wish to call *any* lvgl function from other threads/tasks
- * you should lock on the very same semaphore! */
 SemaphoreHandle_t xGuiSemaphore;
 
-static void guiTask(void *pvParameter) {
-
+_Noreturn static void guiTask(void *pvParameter) {
     (void) pvParameter;
     xGuiSemaphore = xSemaphoreCreateMutex();
 
+    // Initialize lvgl
     lv_init();
-
-    /* Initialize SPI or I2C bus used by the drivers */
     lvgl_driver_init();
 
+    // Initialize draw buffer
     lv_color_t *buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
     assert(buf1 != NULL);
-
-    /* Use double buffered when not working with monochrome displays */
-#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
     lv_color_t *buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
     assert(buf2 != NULL);
-#else
-    static lv_color_t *buf2 = NULL;
-#endif
-
     static lv_disp_draw_buf_t disp_buf;
-
     uint32_t size_in_px = DISP_BUF_SIZE;
-
-#if defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_IL3820         \
-    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_JD79653A    \
-    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_UC8151D     \
-    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_SSD1306
-
-    /* Actual size in pixels, not bytes. */
-    size_in_px *= 8;
-#endif
-
-    /* Initialize the working buffer depending on the selected display.
-     * NOTE: buf2 == NULL when using monochrome displays. */
     lv_disp_draw_buf_init(&disp_buf, buf1, buf2, size_in_px);
 
+    // Initialize display driver
     lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
     disp_drv.flush_cb = disp_driver_flush;
-
-#if defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT || defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT_INVERTED
-    disp_drv.rotated = 1;
-#endif
-
     disp_drv.draw_buf = &disp_buf;
     disp_drv.hor_res = CONFIG_LV_HOR_RES_MAX;
     disp_drv.ver_res = CONFIG_LV_VER_RES_MAX;
+#if defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT || defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT_INVERTED
+    disp_drv.rotated = 1;
+#endif
     lv_disp_drv_register(&disp_drv);
 
-    /* Create and start a periodic timer interrupt to call lv_tick_inc */
+    // Create and start a periodic timer interrupt to call lv_tick_inc
     const esp_timer_create_args_t periodic_timer_args = {
             .callback = &lv_tick_task,
             .name = "periodic_gui"
@@ -111,44 +72,44 @@ static void guiTask(void *pvParameter) {
     ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
 
-    lv_example_get_started_1();
-    lv_timer_create(angle_refresh_task, 100, NULL);
+    // Create the UI and UI refresh task
+    create_ui();
+    lv_timer_create(ui_refresh_task, 100, NULL);
 
+    // Event loop
     while (1) {
-        /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
         vTaskDelay(pdMS_TO_TICKS(10));
-
-        /* Try to take the semaphore, call lvgl related function on success */
         if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
             lv_task_handler();
             lv_timer_handler();
             xSemaphoreGive(xGuiSemaphore);
         }
     }
-
     /* A task should NEVER return */
     free(buf1);
-#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
     free(buf2);
-#endif
     vTaskDelete(NULL);
 }
 
 static lv_obj_t *img;
+static lv_obj_t *label;
 
-static void angle_refresh_task() {
+static void ui_refresh_task() {
     uint16_t angle = lv_img_get_angle(img);
     angle += 10;
     if (angle >= 3600) {
         angle = 0;
     }
-    lv_img_set_angle(img, angle);
+    lv_img_set_angle(img, (int16_t) angle);
+    // Format the angle to a string
+    char buf[10];
+    snprintf(buf, 10, "%d", angle / 10);
+    lv_label_set_text(label, buf);
 }
 
-static void lv_example_get_started_1(void)
+static void create_ui(void)
 {
     LV_IMG_DECLARE(arrow);
-    /*Change the active screen's background color*/
     lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0xffffff), LV_PART_MAIN);
     lv_obj_set_layout(lv_scr_act(), LV_LAYOUT_FLEX);
     lv_obj_set_flex_flow(lv_scr_act(), LV_FLEX_FLOW_COLUMN);
@@ -160,7 +121,7 @@ static void lv_example_get_started_1(void)
     lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
 
     /*Create a white label, set its text and align it to the center*/
-    lv_obj_t * label = lv_label_create(lv_scr_act());
+    label = lv_label_create(lv_scr_act());
     lv_label_set_text(label, "1000m");
     lv_obj_set_style_text_color(lv_scr_act(), lv_color_hex(0x000000), LV_PART_MAIN);
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
