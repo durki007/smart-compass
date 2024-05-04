50ee836261cb27bdabca7ee9d237ba4531ad966f
durki <michal@durkalec.com>
2024-05-04 14:47:18 +0200

    Create global compass_data variable and data type

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,13 +1,26 @@
+#include "freertos/FreeRTOS.h"
+#include <freertos/semphr.h>
 #include "esp_log.h"
-#include "nvs_flash.h"
 
 #include "sc_ble.h"
 #include "sc_display.h"
+#include "compass_data.h"
 
+compass_data_t compass_data;
 
 void
 app_main(void)
 {
+    compass_data = (compass_data_t) {
+            .mutex = (SemaphoreHandle_t) xSemaphoreCreateMutex(),
+            .position = (compass_position_t) {
+                    .lat = 0.0,
+                    .lon = 0.0
+            },
+            .path = (compass_path_t) {
+                    .length = 0
+            }
+    };
     ESP_LOGI("main", "BLE init");
     sc_ble_init();
     ESP_LOGI("main", "Display init");
