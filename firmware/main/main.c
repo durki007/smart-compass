b7206d39184091669181db86a52b1f1c7a117ef6
durki <michal@durkalec.com>
2024-05-05 16:15:03 +0200

    Add back compass data logging

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -9,9 +9,23 @@
 #include "sc_compass.h"
 
 
+compass_data_t compass_data;
+
+void log_compass_data() {
+    compass_data_t *compass_data_ptr = &compass_data;
+    compass_path_t *path = &compass_data_ptr->path;
+    if (xSemaphoreTake(compass_data_ptr->mutex, portMAX_DELAY) == pdTRUE) {
+        ESP_LOGI("COMPASS_DATA", "Position: %f, %f", compass_data_ptr->position.lat, compass_data_ptr->position.lon);
+        ESP_LOGI("COMPASS_DATA", "Path length: %lu", path->length);
+        for (uint32_t i = 0; i < path->length; ++i) {
+            ESP_LOGI("COMPASS_DATA", "Node %lu: %f, %f", i, path->nodes[i].lat, path->nodes[i].lon);
+        }
+        xSemaphoreGive(compass_data_ptr->mutex);
+    }
+}
+
 void
-app_main(void)
-{
+app_main(void) {
     compass_data = (compass_data_t) {
             .mutex = (SemaphoreHandle_t) xSemaphoreCreateMutex(),
             .position = (compass_position_t) {
