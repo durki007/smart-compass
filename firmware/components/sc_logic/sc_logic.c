cef6fd2a88f18c2a94ab4d9936b160e4dce81efc
durki <michal@durkalec.com>
2024-05-26 21:48:47 +0200

    Refactoring and type safety

--- firmware/components/sc_logic/sc_logic.c
+++ firmware/components/sc_logic/sc_logic.c
@@ -9,7 +9,6 @@
 // Defines
 #define TAG "SC_LOGIC"
 #define NODE_DETECTION_PRECISION_M 5
-#define BEARING_OFFSET_DEGREES 0
 
 compass_data_t *compass_data_ptr;
 display_data_t *display_data_ptr;
@@ -30,39 +29,31 @@ static float get_avg_reading() {
     return sum / 5;
 }
 
-static void calculate_lat_lon(float *lat_km, float *lon_km) {
-    float lon_diff = compass_data_ptr->path.nodes[display_data_ptr->next_wp].lon - compass_data_ptr->position.lon;
-    float lat_diff = compass_data_ptr->path.nodes[display_data_ptr->next_wp].lat - compass_data_ptr->position.lat;
-
-    *lat_km = lat_diff * 110.574;
-    *lon_km = lon_diff * 111.320 * cos(compass_data_ptr->path.nodes[display_data_ptr->next_wp].lat * M_PI / 180);
-}
-
 static int16_t radian_to_degree(float angle) {
-    float deg = angle * 180 / M_PI;
+    float deg = (float) (angle * 180 / M_PI);
     while (deg < 0) {
         deg += 360;
     }
     while (deg > 360) {
         deg -= 360;
     }
-    return (int16_t) deg * 10;
+    return (int16_t) (deg * 10);
 }
 
 static int16_t calculate_angle() {
 
-    float curr_lon = compass_data_ptr->position.lon * M_PI / 180;
-    float curr_lat = compass_data_ptr->position.lat * M_PI / 180;
-    float goal_lon = compass_data_ptr->path.nodes[display_data_ptr->next_wp].lon * M_PI / 180;
-    float goal_lat = compass_data_ptr->path.nodes[display_data_ptr->next_wp].lat * M_PI / 180;
+    float curr_lon = (float) (compass_data_ptr->position.lon * M_PI / 180);
+    float curr_lat = (float) (compass_data_ptr->position.lat * M_PI / 180);
+    float goal_lon = (float) (compass_data_ptr->path.nodes[display_data_ptr->next_wp].lon * M_PI / 180);
+    float goal_lat = (float) (compass_data_ptr->path.nodes[display_data_ptr->next_wp].lat * M_PI / 180);
 
     float dLon = (goal_lon - curr_lon);
 
-    float y = sin(dLon) * cos(goal_lat);
-    float x = cos(goal_lat) * sin(goal_lat) - sin(curr_lat)
-                                              * cos(goal_lat) * cos(dLon);
+    float y = sinf(dLon) * cosf(goal_lat);
+    float x = cosf(goal_lat) * sinf(goal_lat) - sinf(curr_lat)
+                                                * cosf(goal_lat) * cosf(dLon);
 
-    float angle_radians = atan2(y, x);
+    float angle_radians = atan2f(y, x);
 
     float bearing_radians = compass_data_ptr->bearing;
     add_reading_to_avg_buf(angle_radians - bearing_radians);
@@ -83,10 +74,11 @@ static uint16_t calculate_distance() {
     float lon_diff = compass_data_ptr->path.nodes[display_data_ptr->next_wp].lon - compass_data_ptr->position.lon;
     float lat_diff = compass_data_ptr->path.nodes[display_data_ptr->next_wp].lat - compass_data_ptr->position.lat;
 
-    float lat_km = lat_diff * 110.574;
-    float lon_km = lon_diff * 111.320 * cos(compass_data_ptr->path.nodes[display_data_ptr->next_wp].lat * M_PI / 180);
+    float lat_km = (float) (lat_diff * 110.574);
+    float lon_km = (float) (lon_diff * 111.320 *
+                            cos(compass_data_ptr->path.nodes[display_data_ptr->next_wp].lat * M_PI / 180));
 
-    return (uint32_t) (sqrt(lat_km * lat_km + lon_km * lon_km) * 1000);
+    return (uint32_t) (sqrtf(lat_km * lat_km + lon_km * lon_km) * 1000);
 }
 
 _Noreturn static void logic_task() {
